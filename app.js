16047a9c3bc06681b8cb2c884f654392a56e36d0
Andy Green <aj.green07@outlook.com>
2020-08-28 23:19:40 +0100

    First draft

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+// variables
+const instructions = document.getElementById("instructions");
+const play = document.getElementById("play");
+const image = document.getElementById("image");
+const game = document.getElementById("game");
+const health = document.getElementById("health");
+const happiness = document.getElementById("happiness");
+const option1 = document.getElementById("option1");
+
 const creature = {
     name: "Gizmo",
     typeOfPet: "Mogwai",
-    health: 10,
-    happiness: 10,
+    health: 5,
+    happiness: 5,
     // functions
     mirror() {
         this.health --;
@@ -12,10 +21,12 @@ const creature = {
     read() {
         this.happiness ++;
         // image reading
+        image.src = `img/reading.png`;
     },
     music() {
         this.health ++;
         // keyboard image
+        image.src = `img/music.png`;
     },
     sleep() {
         this.health ++;
@@ -29,9 +40,38 @@ const creature = {
     drink() {
         this.health --;
         this.happiness --;
+        image.src = `img/spill_drink.png`;
     },
 }
 
-console.log(typeof(creature));
-creature.mirror();
-console.log(creature.health, creature.happiness);
+// main content
+play.addEventListener("click", () => {
+    game.style.display = "block";
+    instructions.style.display = "none";
+})
+
+const updateStats = () => {
+    health.textContent = creature.health;
+    happiness.textContent = creature.happiness;
+}
+
+// Options
+option1.addEventListener("click", () => {
+    creature.music();
+    updateStats();
+})
+
+option2.addEventListener("click", () => {
+    creature.read();
+    updateStats();
+})
+
+option3.addEventListener("click", () => {
+    creature.drink();
+    updateStats();
+})
+
+
+// console.log(typeof(creature));
+// creature.mirror();
+// console.log(creature.health, creature.happiness);
